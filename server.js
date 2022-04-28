98eea2c3e31d5000e879dc93d3848f25bdb82c88
Andrew <andrew.jiang81@gmail.com>
2022-04-28 18:42:57 +1000

    Detect Changes on Hermes and Push to Slack

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+const puppeteer = require('puppeteer');
+require('events').EventEmitter.prototype._maxListeners = 70;
+require('events').defaultMaxListeners = 70;
 
 //Express configuration
 const app = express();
@@ -11,88 +14,113 @@ const PORT = process.env.PORT || 3000;
 
 
 //Main configuration variables
-const urlToCheck = `http://urlyouwant.com/tocheck`;
-const elementsToSearchFor = ['Text you want to watch for', 'imageYouWantToCheckItsExistence.png'];
-const checkingFrequency = 5 * 60000; //first number represent the checkingFrequency in minutes
-
+const urlToCheck = `https://www.hermes.com/au/en/category/women/bags-and-small-leather-goods/bags-and-clutches/#|`;
+const elementsToSearchFor = ['1'];
+const checkingFrequency = 0.2 * 60000; //first number represent the checkingFrequency in minutes
+let initial_product_count = 4
 //Slack Integration
-const SLACK_WEBHOOK_URL = 'https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX';
+const SLACK_WEBHOOK_URL = 'https://hooks.slack.com/services/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';
 const slack = require('slack-notify')(SLACK_WEBHOOK_URL);
 
-//SendGrid Email Integration
-const SENDGRID_APY_KEY = 'AA.AAAA_AAAAAAAAAAAAA.AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
-const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey(SENDGRID_APY_KEY);
-const emailFrom = 'aaa@aaa.com';
-const emailsToAlert = ['emailOneToSend@theAlert.com', 'emailTwoToSend@theAlert.com'];
+// //SendGrid Email Integration
+// const SENDGRID_APY_KEY = 'AA.AAAA_AAAAAAAAAAAAA.AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
+// const sgMail = require('@sendgrid/mail');
+// sgMail.setApiKey(SENDGRID_APY_KEY);
+// const emailFrom = 'aaa@aaa.com';
+// const emailsToAlert = ['emailOneToSend@theAlert.com', 'emailTwoToSend@theAlert.com'];
 
 
 const checkingNumberBeforeWorkingOKEmail = 1440 / (checkingFrequency / 60000);   //1 day = 1440 minutes
 let requestCounter = 0;
 
-
 //Main function
 const intervalId = setInterval(function () {
+    
+    (async () => {
+        /* Initiate the Puppeteer browser */
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto(`https://www.hermes.com/au/en/category/women/bags-and-small-leather-goods/bags-and-clutches/#|`, { waitUntil: "networkidle0"})
 
-    request(urlToCheck, function (err, response, body) {
-        //if the request fail
-        if (err) {
-            console.log(`Request Error - ${err}`);
+        await page.waitForSelector('li.product-grid-list-item')
+        const elements = await page.$$('li.product-grid-list-item');
+
+        if(elements.length != initial_product_count){
+                console.log(elements.length)
+                //Slack Alert Notification
+                await slack.alert(`🔥🔥🔥  <${urlToCheck}/|Change detected in ${urlToCheck}, product list now contains ${elements.length} products>  🔥🔥🔥 `, function (err) {
+                    if (err) {
+                        console.log('Slack API error:', err);
+                    } else {
+                        console.log('Message received in slack!');
+                    }
+                });
+                initial_product_count = elements.length
+              
         }
-        else {
-            //if the target-page content is empty
-            if (!body) {
-                console.log(`Request Body Error - ${err}`);
-            }
-            //if the request is successful
-            else {
+        return browser
+  
+    })().then(function(value) {value.close()}, function(error) {})
 
-                //if any elementsToSearchFor exist
-                if (elementsToSearchFor.some((el) => body.includes(el))) {
+    // request(urlToCheck, function (err, response, body) {
+    //     //if the request fail
+    //     if (err) {
+    //         console.log(`Request Error - ${err}`);
+    //     }
+    //     else {
+    //         //if the target-page content is empty
+    //         if (!body) {
+    //             console.log(`Request Body Error - ${err}`);
+    //         }
+    //         //if the request is successful
+    //         else {
+    //             console.log(body)
+    //             //if any elementsToSearchFor exist
+    //             if (elementsToSearchFor.some((el) => body.includes(el))) {
 
-                    // Slack Alert Notification
-                    slack.alert(`🔥🔥🔥  <${urlToCheck}/|Change detected in ${urlToCheck}>  🔥🔥🔥 `, function (err) {
-                        if (err) {
-                            console.log('Slack API error:', err);
-                        } else {
-                            console.log('Message received in slack!');
-                        }
-                    });
+                    // // Slack Alert Notification
+                    // slack.alert(`🔥🔥🔥  <${urlToCheck}/|Change detected in ${urlToCheck}>  🔥🔥🔥 `, function (err) {
+                    //     if (err) {
+                    //         console.log('Slack API error:', err);
+                    //     } else {
+                    //         console.log('Message received in slack!');
+                    //     }
+                    // });
 
-                    // Email Alert Notification
-                    const msg = {
-                        to: emailsToAlert,
-                        from: emailFrom,
-                        subject: `🔥🔥🔥 Change detected in ${urlToCheck} 🔥🔥🔥`,
-                        html: `Change detected in <a href="${urlToCheck}"> ${urlToCheck} </a>  `,
-                    };
-                    sgMail.send(msg)
-                        .then(()=>{console.log("Alert Email Sent!");})
-                        .catch((emailError)=>{console.log(emailError);});
-                }
+    //                 // // Email Alert Notification
+    //                 // const msg = {
+    //                 //     to: emailsToAlert,
+    //                 //     from: emailFrom,
+    //                 //     subject: `🔥🔥🔥 Change detected in ${urlToCheck} 🔥🔥🔥`,
+    //                 //     html: `Change detected in <a href="${urlToCheck}"> ${urlToCheck} </a>  `,
+    //                 // };
+    //                 // sgMail.send(msg)
+    //                 //     .then(()=>{console.log("Alert Email Sent!");})
+    //                 //     .catch((emailError)=>{console.log(emailError);});
+    //             }
 
-            }
-        }
-    });
+    //         }
+    //     }
+    // });
 
     requestCounter++;
 
 
-    // "Working OK" email notification logic
-    if (requestCounter > checkingNumberBeforeWorkingOKEmail) {
+    // // "Working OK" email notification logic
+    // if (requestCounter > checkingNumberBeforeWorkingOKEmail) {
 
-        requestCounter = 0;
+    //     requestCounter = 0;
 
-        const msg = {
-            to: emailsToAlert,
-            from: emailFrom,
-            subject: '👀👀👀 Website Change Monitor is working OK 👀👀👀',
-            html: `Website Change Monitor is working OK - <b>${new Date().toLocaleString("en-US", {timeZone: "America/New_York"})}</b>`,
-        };
-        sgMail.send(msg)
-            .then(()=>{console.log("Working OK Email Sent!");})
-            .catch((emailError)=>{console.log(emailError);});
-    }
+    //     const msg = {
+    //         to: emailsToAlert,
+    //         from: emailFrom,
+    //         subject: '👀👀👀 Website Change Monitor is working OK 👀👀👀',
+    //         html: `Website Change Monitor is working OK - <b>${new Date().toLocaleString("en-US", {timeZone: "America/New_York"})}</b>`,
+    //     };
+    //     sgMail.send(msg)
+    //         .then(()=>{console.log("Working OK Email Sent!");})
+    //         .catch((emailError)=>{console.log(emailError);});
+    // }
 
 }, checkingFrequency);
 
