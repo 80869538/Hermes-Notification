a6092c46ba2e12d8b1b3081db775a1546633b644
Andrew <andrew.jiang81@gmail.com>
2022-04-29 11:06:51 +1000

    add output

--- server.js
+++ server.js
@@ -40,6 +40,7 @@ const intervalId = setInterval(function () {
         /* Initiate the Puppeteer browser */
         const browser = await puppeteer.launch({args: ['--no-sandbox']});
         const page = await browser.newPage();
+        console.log("fetch page")
         await page.goto(`https://www.hermes.com/au/en/category/women/bags-and-small-leather-goods/bags-and-clutches/#|`, { waitUntil: "networkidle0"})
 
         await page.waitForSelector('li.product-grid-list-item')
